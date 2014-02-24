5dd66d98ed6aae07ff209f868f516d5e800f9ac2
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-02-24 09:57:06 +0100

    Initial commit

---
+++ eSolution/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var mainmenu = {};	// @menuItem
+	var login1 = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	mainmenu.click = function mainmenu_click (event)// @startlock
+	{// @endlock
+		prompt("en ruta som kommer upp här");
+	};// @lock
+
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		alert ("thanks for being here"); 
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		alert ("success"); 
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("mainmenu", "click", mainmenu.click, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+// @endregion
+};// @endlock
